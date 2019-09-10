c8d6424430914dcfe70fc72d47dc0d878d794f4d
Chris Stefano <virtualstaticvoid@gmail.com>
2019-09-10 10:56:07 +0100

    upgrade to protobuf version 3

--- protobuf/internal.pb.go
+++ protobuf/internal.pb.go
@@ -20,7 +20,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 // represents a message with generic protobuf payload
 // this is an internal type used for storing messages
@@ -186,78 +186,12 @@ func (m *Event) GetCreatedAt() *timestamp.Timestamp {
 	return nil
 }
 
-// XXX_OneofFuncs is for the internal use of the proto package.
-func (*Event) XXX_OneofFuncs() (func(msg proto.Message, b *proto.Buffer) error, func(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error), func(msg proto.Message) (n int), []interface{}) {
-	return _Event_OneofMarshaler, _Event_OneofUnmarshaler, _Event_OneofSizer, []interface{}{
+// XXX_OneofWrappers is for the internal use of the proto package.
+func (*Event) XXX_OneofWrappers() []interface{} {
+	return []interface{}{
 		(*Event_SystemContext)(nil),
 		(*Event_TenantContext)(nil),
 	}
-}
-
-func _Event_OneofMarshaler(msg proto.Message, b *proto.Buffer) error {
-	m := msg.(*Event)
-	// context
-	switch x := m.Context.(type) {
-	case *Event_SystemContext:
-		b.EncodeVarint(3<<3 | proto.WireBytes)
-		if err := b.EncodeMessage(x.SystemContext); err != nil {
-			return err
-		}
-	case *Event_TenantContext:
-		b.EncodeVarint(4<<3 | proto.WireBytes)
-		if err := b.EncodeMessage(x.TenantContext); err != nil {
-			return err
-		}
-	case nil:
-	default:
-		return fmt.Errorf("Event.Context has unexpected type %T", x)
-	}
-	return nil
-}
-
-func _Event_OneofUnmarshaler(msg proto.Message, tag, wire int, b *proto.Buffer) (bool, error) {
-	m := msg.(*Event)
-	switch tag {
-	case 3: // context.system_context
-		if wire != proto.WireBytes {
-			return true, proto.ErrInternalBadWireType
-		}
-		msg := new(SystemContext)
-		err := b.DecodeMessage(msg)
-		m.Context = &Event_SystemContext{msg}
-		return true, err
-	case 4: // context.tenant_context
-		if wire != proto.WireBytes {
-			return true, proto.ErrInternalBadWireType
-		}
-		msg := new(TenantContext)
-		err := b.DecodeMessage(msg)
-		m.Context = &Event_TenantContext{msg}
-		return true, err
-	default:
-		return false, nil
-	}
-}
-
-func _Event_OneofSizer(msg proto.Message) (n int) {
-	m := msg.(*Event)
-	// context
-	switch x := m.Context.(type) {
-	case *Event_SystemContext:
-		s := proto.Size(x.SystemContext)
-		n += 1 // tag and wire
-		n += proto.SizeVarint(uint64(s))
-		n += s
-	case *Event_TenantContext:
-		s := proto.Size(x.TenantContext)
-		n += 1 // tag and wire
-		n += proto.SizeVarint(uint64(s))
-		n += s
-	case nil:
-	default:
-		panic(fmt.Sprintf("proto: unexpected type %T in oneof", x))
-	}
-	return n
 }
 
 func init() {
